7dd9dd17d4458b109f3746d9a676227764ee880e
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-10-18 09:56:01 -0400

    new files and put the functions in string reader

---
+++ triviaGame/content.js
@@ -0,0 +1,62 @@
+const string = `Jake is smart; true;
+The Earth is Flat; false;
+Jake can't not roste; false;
+Working in this class is helps; true;
+Pluto is not a normal planet; true;
+Global warming isnt real; false;
+bowen is not not not not not not smart; true;
+Jake is a good driver; true;
+The sun is hot; true;
+Aidan is not lazy; fase;
+when working with Aidan litle work gets done;
+Mike isnt a total beast with monty in ros;
+piggins were made by the government
+bad dragonis comoto dragon breeding company with the goal of making them a domesticated pet;
+The earth is not flat;
+The earth is not a perfect sphere;
+Radiation kills;
+Mike can spell much good;
+global warming is bad;
+memphis = kim john un;
+fire herts;
+aidan = bad at siege;
+memphis is secretly tall but has prosthetic legs;
+Aidan noob rages on rainbow 6 siege (and sucks at it);
+euan is memphis's twin and took his legs;
+it smells like stale diretos;
+Emma is tall
+it is raining outside
+Aidan is an idiot
+Bowen is cool
+Memphis cant reach the top shelf
+lamorie has baby legs
+bowen has aidans wallet
+Mike cant spell refrigir8tors
+mr malorie secretly works for nasa
+jake has a shark neckless
+jake like pizza
+it is still summer
+jakes computer is a dcu
+aidan has a youtube channel called blaze conroy
+sharks have arms
+Bowen doesnt have glasses
+kade has a crush on brad
+euan is swim
+mike is trash
+aidan is the better leader
+aidan has a future
+TBB is best
+Lamorie is cool
+mike is here legaly
+alienwear is a good brand
+the sky is blue
+Lamorie has baby legs
+Jake is a noob
+PC masterace
+jake is a good driver
+Mike is not a squeaker
+aidan has a wallet
+gaming laptops are good
+mike can do basic motor functions
+siege has small maps
+mike is cool`
