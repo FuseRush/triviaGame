eaebfbe451157f53892fced6b428263e4f2c877f
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-10-23 14:12:15 -0400

    getting makeQuestions farther

--- triviaGame/gamefunctions.js
+++ triviaGame/gamefunctions.js
@@ -60,3 +60,59 @@ function orEval(condition1, condition2){
   }
   return retObj;
 }
+
+
+//to the following:
+/* arrayReader(array, split)
+take an array of strings in the format of text 1 split text 2
+text 2 is assumed to be the words true or false. Uses these to set bools
+makes a new array where each element is an object.
+@param array: {array} an array with string containing the split
+@param split: {string} the item to split at
+@return {array} an array of objects keyed with str, bool and the new key negated: false
+*/
+
+//2. implement
+/* notEval(condition)
+takes a condtion, negates it's value, adds the text "it is not the case that" to the
+front of it, sets the negated key to true
+@param condtion {object} an object with str and bool keys
+@return {object} an object with str, bool, and negated keys
+*/
+
+//3. implement the following
+/* makeQuestion(conditions, maxDepth=3, negate=.2)
+takes an array of condition objects formated {str: text, bool: bool, negated: bool}
+and uses it make a question of depths between 1 - maxDepth combined statement length
+@param condition {array} an array of objects formatted as listed above
+@param maxDepth {int} number of conditionals to be put together at max
+@param negate {float} chance of a negation happening
+@return a new object with the same general format
+*/
+function makeQuestions(conditions, maxDepth=3, negate=.2){
+  var retObj = {};
+  var str = conditions.str;
+  var bool = conditions.bool;
+    for (var i = 0; i < maxDepth - 1; i++) {
+      if (Math.random() < .5){
+      orEval(condtions[i], conditions[i]);
+    }else {
+      andEval(conditions[i], conditions[i]);
+    }
+  }
+  return retObj;
+}
+//^^^ with this one -
+//You'll need to pick random things without replacement (did we do a function for that?)
+//You'll need to have a random chance of a negation (by default 20% (aka .2)
+//You'll need to loop through maxDepth amount of things and glue them together, but you'll have to do something different when there is only 1 left . What sort of loop and logic would work best for this?
+
+//4. implement the following
+/* makeSentence(condition)
+Makes a (likely run-on) sentence out of a conditional stored in an object with the keys str,
+bool, and negated. It does the following: if negated then it capitalized the i in
+"it is not the case" and adds a period to the end of the str. If it is not
+negated then it adds the phrase "It is the case " to the start of the str and adds a period to the end of the string.
+@param condition {array} an array of objects formatted as listed above
+@return {object} a new object with the same general format
+*/
