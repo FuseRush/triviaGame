1f25ce81c717e0d810914704022bc05c10fb3548
Lilla <dilillahall0802@gmail.com>
2019-10-24 09:08:13 -0500

    pulling

--- triviaGame/gamefunctions.js
+++ triviaGame/gamefunctions.js
@@ -59,24 +59,41 @@ or state and combines the text of the two conditions. Cleans up text some
 function orEval(condition1, condition2){
   var retObj = {};
   if (condition1.bool == true || condition2.bool == true ) {
-    retObj = {str: condition1.str + " or " + condition2.str, bool: true};
+    retObj = {str: condition1.str + " or " + condition2.str, bool:true};
     }else {
-    retObj = {str: condition1.str + " or " + condition2.str, bool: false};
+    retObj = {str: condition1.str + " or " +condition2.str, bool:false};
   }
-  return retObj
+  return retObj;
 }
-
 /* notEval(condition)
 takes a condtion, negates it's value, adds the text "it is not the case that" to the
 front of it, sets the negated key to true
 @param condtion {object} an object with str and bool keys
 @return {object} an object with str, bool, and negated keys
 */
-function notEval(condition) {
-  var retObj = {};
-  if (condition.bool == false) {
-    retObj = {str: condition.str, bool: true};
+
+
+/* makeQuestion(conditions, maxDepth=3, negate=.2)
+takes an array of condition objects formated {str: text, bool: bool, negated: bool}
+and uses it make a question of depths between 1 - maxDepth combined statement length
+it should randomly use and or ors at a 50% chance each
+@param condition {array} an array of objects formatted as listed above
+@param maxDepth {int} number of conditionals to be put together at max
+@param negate {float} chance of a negation happening
+@return a new object with the same general format
+*/
+
+/* makeSentence(condition)
+Makes a (likely run-on) sentence out of a conditional stored in an object with the keys
+str, bool, and negated. It does the following: if negated then it capitalized the i in
+"it is not the case" and adds a period to the end of the str. If it is not negated then it
+adds the phrase "It is the case " to the start of the str and adds a period to the end of the string.
+@param condition {array} an array of objects formatted as listed above
+@return {object} a new object with the same general format
+*/
+function makeSentence(condition) {
+  if (condition.negated== true ){
+    condition.str
   }
-  console.log(retObj);
-  return "It is not the case that " + retObj.str
+  {str: text, bool: bool, negated: bool}
 }
