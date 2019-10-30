164d6e49950ee9e3e49e391378db5b7e9fdb4a6d
Lilla <dilillahall0802@gmail.com>
2019-10-30 09:15:01 -0500

    help

--- triviaGame/gamefunctions.js
+++ triviaGame/gamefunctions.js
@@ -1,4 +1,4 @@
-/* randNum(max);
+'cccccfvVVVVVVVVVVVV  '/* randNum(max);
 a helper function to return whole number randoms
 @param max {int} the max in the random range
 @return {int} a random number of 1 or more
@@ -71,7 +71,7 @@ front of it, sets the negated key to true
 @return {object} an object with str, bool, and negated keys
 */
 function notEval(condition) {
-  return {str: "It is not the case that " + condition.str, bool: !condition.bool, negated: !conditon.negated}
+  return {str: "It is not the case that " + condition.str, bool: !condition.bool, negated: true}
 }
 //to the following:
 /* arrayReader(array, split)
@@ -85,7 +85,6 @@ makes a new array where each element is an object.
 
 //3. implement the following
 /* makeQuestion(conditions, maxDepth=3, negate=.2)
-makeQuestion(stringReader(content))
 takes an array of condition objects formated {str: text, bool: bool, negated: bool}
 and uses it make a question of depths between 1 - maxDepth combined statement length
 it should randomly use and or ors at a 50% chance each
@@ -93,46 +92,27 @@ it should randomly use and or ors at a 50% chance each
 @param maxDepth {int} number of conditionals to be put together at max
 @param negate {float} chance of a negation happening
 @return a new object with the same general format
-
-1. make a return object
-2. find out the actual depth
-  if depth is 1... pick one thing at random off condition...check if we negate, return;
-  otherwise - 1.  pick our things - uniqueIndex(conditions.length, depth);
-              2. start doing the big return
-                  1. go make a for loop (things.length -1)
-                  2. in the for loop want to determine a and or or
-                  [42, 11, 27, 32, 0]
-                  we need 2 things, but we make a for lop we get 1 thing =/
-                  if we use i for our iterator we caould overcome this one of two ways.
-                    way #1 i and i+1 make our loop using length -2 rather than -1
-                    way #2 i and i-1 we could set i to 1 initially or if i = 0 continue;
-                      why are ways 1 & 2 kind of bad?
-
-                        first step is to pick first two;
-                          Ex 42, 11
-                        the second set and there on.
-                          Ex 11, 27
-                        so if we were to fix that... we'd need to come up with some
-                        way to skip a sentence.
-                        if we do that, when all is said and done, we still have
-                        to add all the sets together.
-                          Ex {42,11} {23, 32}
-                        we would have to come up with a way of dealing with having
-                        an uneven number of things.
-
-                    way #3 for i in length of things pop off the end, we will have
-                      to treat the first one different as with way 2 above
-                      way #3 when we have the first thing check if it is negated
-                      make the retObj = to the first thing
-                      following the first thing figure out and or or and negation
-                      retObj = which ever eval(new item, retObj)
-              3. return retObj
-
 */
-
 function makeQuestions(conditions, maxDepth=3, negate=.2){
   var retObj = {};
-
+  var int = uniqueIndex(conditions.length, randNum(maxDepth));
+  // gives me a random number of numbers based on the number of conditions
+  for (var i = 0; i < int.length; i++) {
+    for (var j = 0; j < maxDepth - 1; j++) {
+      if (Math.random() < .5){
+       var orEvl = orEval(conditions[int[i]], conditions[int[j]]);
+       retObj = orEvl;
+    }else {
+      var andEvl = andEval(conditions[int[i]], conditions[int[j]]);
+      retObj = andEvl;
+    }
+  }
+}
+  if (conditions.negated == true) {
+   var notEvl = notEval(cond);
+   retObj = notEvl;
+  }
+  return retObj;
 }
 //^^^ with this one -
 //You'll need to pick random things without replacement (did we do a function for that?)
@@ -149,7 +129,6 @@ adds the phrase "It is the case " to the start of the str and adds a period to t
 @return {object} a new object with the same general format
 */
 function makeSentence(condition) {
-<<<<<<< HEAD
   var retObj = {}
   if (retObj.negated == true){
     retObj = {
@@ -167,12 +146,3 @@ function makeSentence(condition) {
   }
     return retObj
 }
-=======
-  if (condition.negated) {
-    condition.str = "I" + condition.str.slice(1) + ".";
-    }else{
-  condition.str = "It is not the case " + condition.str + ".";
-    }
-    return condition
-  }
->>>>>>> e6872fd540a9f965debf05a13b60e6e46c2b5123
